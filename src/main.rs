b509a93b956b9c5522ae64ccf566558240e6238f
Anurag Maurya <justanuragmaurya@gmail.com>
2026-01-29 22:55:08 +0530

    return a predefined response to each request on the tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use std::{io::Read, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener};
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
@@ -9,7 +9,19 @@ fn main(){
 
         let n = stream.read(&mut buffer).unwrap();
 
-        println!("{}",String::from_utf8_lossy(&buffer[..n]));
+        let request = String::from_utf8_lossy(&buffer[..n]);
+
+        println!("{request}");
+
+        println!("{} {}",5,6);
+
+        let res_body = "Hi take this response";
+
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/plain\r\n\r\n{}",res_body.len(),&res_body);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap()
+
     }
 
 }
